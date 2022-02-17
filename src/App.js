fbe1d3657e52b7b289d9c97c079e06bbc656d884
zero0Halo <steve.e.swanson@gmail.com>
2022-02-17 09:12:51 -0600

    feat: first commit ever

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Hi All. I got this blank template setup for the code test today.</h1>
+    </div>
+  );
+}
+
+export default App;
